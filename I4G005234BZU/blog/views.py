27a221db72ba6b31dca0cf5e6f2ac3eefe62dd57
knightwalkha <ohiosumuankehinde@gmail.com>
2022-06-23 23:41:26 +0100

    updated with teplates folder and some other settings

--- I4G005234BZU/blog/views.py
+++ I4G005234BZU/blog/views.py
@@ -1,3 +1,4 @@
+from msilib.schema import ListView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
@@ -5,10 +6,23 @@ from .models import Post
 
 # Create your views here.
 
+class PostListView(ListView):
+    model = Post
+
 class PostCreateView(CreateView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
 
-class PostDetailView(PostDetailView):
+class PostDetailView(DetailView):
     model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
